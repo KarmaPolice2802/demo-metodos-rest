6d00320ca98f020818cefffe959bcd4296a7dbbd
ahiltonplay <ahiltonplayy@gmail.com>
2025-02-27 10:51:32 -0500

    Primer commit ejercicio 2

---
+++ src/main/java/pe/edu/idat/demometodosrest/Ejercicio2Service.java
@@ -0,0 +1,12 @@
+package pe.edu.idat.demometodosrest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Ejercicio2Service {
+
+    public String SaludoEjercicio2(){
+        return "Hola Git desde mi rama";
+    }
+
+}
