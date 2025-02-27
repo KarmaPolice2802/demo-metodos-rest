ef03af69430a00ce49c650f62f44f023b625c7a2
KarmaPolice2802 <sv46793719@idat.pe>
2025-02-27 10:51:54 -0500

    Primer commit ejercicio 1

---
+++ src/main/java/pe/edu/idat/demometodosrest/Ejercicio1Service.java
@@ -0,0 +1,11 @@
+package pe.edu.idat.demometodosrest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Ejercicio1Service {
+
+    public String SaludoEjercicio1() {
+        return "Hola Git desde mi rama";
+    }
+}
