37187c9fa3af11e93857563b4ea74c5f8ed0d201
ahiltonplay <ahiltonplayy@gmail.com>
2025-02-27 11:38:01 -0500

    Ejercicio 2.3

--- src/main/java/pe/edu/idat/demometodosrest/Ejercicio2Service.java
+++ src/main/java/pe/edu/idat/demometodosrest/Ejercicio2Service.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class Ejercicio2Service {
 
     public String SaludoEjercicio2(){
-        return "Hola Git desde mi rama ej2";
+        return "Hola Git desde mi rama";
     }
 
 }
